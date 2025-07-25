7da2ca5a5385a25aa0dcf57c4d4525336925c297
bluisblu <53455507+bluisblu@users.noreply.github.com>
2025-07-24 23:16:45 -0400

    iScrFx begin and end functions

--- src/SB/Core/gc/iScrFX.cpp
+++ src/SB/Core/gc/iScrFX.cpp
@@ -1,5 +1,6 @@
 #include "iScrFX.h"
 #include "rwcore.h"
+#include "rwplcore.h"
 
 extern RwRaster* g_rast_gctapdance;
 extern S32 g_alreadyTriedAlloc;
@@ -7,6 +8,10 @@ extern _iMotionBlurData sMBD;
 extern U32 sMotionBlurEnabled;
 
 void iScrFxLensFlare_Reset()
+{
+}
+
+void RenderRect(RwRect* rect, F32 unknown)
 {
 }
 
@@ -29,6 +34,7 @@ void iScrFxCameraCreated(RwCamera* pCamera)
 {
     sMBD.motionBlurAlpha = 0x90;
     sMBD.motionBlurFrontBuffer = NULL;
+    // for some reason these are 0x40 ahead of target?
     sMBD.index[0] = 0;
     sMBD.index[1] = 1;
     sMBD.index[2] = 2;
@@ -37,8 +43,32 @@ void iScrFxCameraCreated(RwCamera* pCamera)
     sMBD.index[5] = 3;
 }
 
+void iScrFxEnd()
+{
+    RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEFOGENABLE,
+                                                   (void*)rwFOGTYPENAFOGTYPE);
+    RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEZTESTENABLE, (void*)true);
+    RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEZWRITEENABLE, (void*)true);
+    RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATETEXTURERASTER, NULL);
+    RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEVERTEXALPHAENABLE, NULL);
+    RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATESRCBLEND, (void*)rwBLENDONE);
+    RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEDESTBLEND, (void*)rwBLENDZERO);
+}
+
 void iScrFxBegin()
 {
+    RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATETEXTUREFILTER,
+                                                   (void*)rwFILTERNEAREST);
+    RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEFOGENABLE, NULL);
+    RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEZTESTENABLE, NULL);
+    RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEZWRITEENABLE, NULL);
+    RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATETEXTURERASTER, NULL);
+    RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEVERTEXALPHAENABLE, (void*)true);
+    RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATESRCBLEND, (void*)rwBLENDSRCALPHA);
+    RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEDESTBLEND,
+                                                   (void*)rwBLENDINVSRCALPHA);
+    RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATECULLMODE,
+                                                   (void*)rwCULLMODECULLNONE);
 }
 
 void iScrFxInit()
