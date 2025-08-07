2d7c208f77b5e4c0f423152be039102ab1206f60
bluisblu <53455507+bluisblu@users.noreply.github.com>
2025-08-07 01:57:14 -0400

    iScrFXCameraEndScene WIP

--- src/SB/Core/gc/iScrFX.cpp
+++ src/SB/Core/gc/iScrFX.cpp
@@ -28,13 +28,63 @@ S32 iScrFxCameraDestroyed(RwCamera* pCamera)
 
 void iScrFxCameraEndScene(RwCamera* pCamera)
 {
+    if ((sMotionBlurEnabled != 0) && (sMBD.motionBlurAlpha != 0))
+    {
+        U8 alpha = sMBD.motionBlurAlpha;
+        if (sMBD.motionBlurFrontBuffer != 0x0)
+        {
+            sMBD.vertex[0xc] = 0xFF;
+            sMBD.vertex[0xd] = 0xFF;
+            sMBD.vertex[0xe] = 0xFF;
+            sMBD.vertex[0xf] = alpha;
+            sMBD.vertex[0x24] = 0xFF;
+            sMBD.vertex[0x25] = 0xFF;
+            sMBD.vertex[0x26] = 0xFF;
+            sMBD.vertex[0x27] = alpha;
+            sMBD.vertex[0x3c] = 0xFF;
+            sMBD.vertex[0x3d] = 0xFF;
+            sMBD.vertex[0x3e] = 0xFF;
+            sMBD.vertex[0x3f] = alpha;
+            sMBD.vertex[0x54] = 0xFF;
+            sMBD.vertex[0x55] = 0xFF;
+            sMBD.vertex[0x56] = 0xFF;
+            sMBD.vertex[0x57] = alpha;
+            RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATETEXTUREFILTER,
+                                                           (void*)rwFILTERNEAREST);
+            RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEFOGENABLE,
+                                                           (void*)rwFOGTYPENAFOGTYPE);
+            RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEZTESTENABLE, (void*)false);
+            RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEZWRITEENABLE, (void*)false);
+            RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATETEXTURERASTER,
+                                                           sMBD.motionBlurFrontBuffer);
+            RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEVERTEXALPHAENABLE,
+                                                           (void*)true);
+            RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATESRCBLEND,
+                                                           (void*)rwBLENDSRCALPHA);
+            RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEDESTBLEND,
+                                                           (void*)rwBLENDINVSRCALPHA);
+            RwEngineInstance->dOpenDevice.fpIm3DRenderIndexedPrimitive(rwPRIMTYPETRILIST,
+                                                                       sMBD.index, 6);
+            RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEFOGENABLE,
+                                                           (void*)rwFOGTYPENAFOGTYPE);
+            RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEZTESTENABLE, (void*)true);
+            RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEZWRITEENABLE, (void*)true);
+            RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATETEXTURERASTER, NULL);
+            RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEVERTEXALPHAENABLE,
+                                                           (void*)false);
+            RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATESRCBLEND,
+                                                           (void*)rwBLENDSRCALPHA);
+            RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEDESTBLEND,
+                                                           (void*)rwBLENDINVSRCALPHA);
+        }
+    }
+    return;
 }
 
 void iScrFxCameraCreated(RwCamera* pCamera)
 {
     sMBD.motionBlurAlpha = 0x90;
     sMBD.motionBlurFrontBuffer = NULL;
-    // for some reason these are 0x40 ahead of target?
     sMBD.index[0] = 0;
     sMBD.index[1] = 1;
     sMBD.index[2] = 2;
@@ -50,7 +100,7 @@ void iScrFxEnd()
     RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEZTESTENABLE, (void*)true);
     RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEZWRITEENABLE, (void*)true);
     RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATETEXTURERASTER, NULL);
-    RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEVERTEXALPHAENABLE, NULL);
+    RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEVERTEXALPHAENABLE, (void*)false);
     RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATESRCBLEND, (void*)rwBLENDONE);
     RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEDESTBLEND, (void*)rwBLENDZERO);
 }
@@ -59,9 +109,10 @@ void iScrFxBegin()
 {
     RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATETEXTUREFILTER,
                                                    (void*)rwFILTERNEAREST);
-    RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEFOGENABLE, NULL);
-    RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEZTESTENABLE, NULL);
-    RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEZWRITEENABLE, NULL);
+    RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEFOGENABLE,
+                                                   (void*)rwFOGTYPENAFOGTYPE);
+    RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEZTESTENABLE, (void*)false);
+    RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEZWRITEENABLE, (void*)false);
     RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATETEXTURERASTER, NULL);
     RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATEVERTEXALPHAENABLE, (void*)true);
     RwEngineInstance->dOpenDevice.fpRenderStateSet(rwRENDERSTATESRCBLEND, (void*)rwBLENDSRCALPHA);
