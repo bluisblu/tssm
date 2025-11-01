1971ebe7654ad69c1e28b53e68599695100b67ef
bluisblu <53455507+bluisblu@users.noreply.github.com>
2025-11-01 10:11:45 -0400

    Undo zWadUI .sbss regression

--- src/SB/Game/zWadUI.cpp
+++ src/SB/Game/zWadUI.cpp
@@ -1,3 +1,13 @@
+#include "zUI.h"
+
+// I believe this is correct?
+// Aren't these initialized to 0 if no initialization is given?
+
+zUIController* UIControl = 0;
+zUIControllerAsset* UIControlAsset = 0;
+zUIUserString* zUIString = 0;
+zUIUserStringAsset* zUIStringAsset = 0;
+
 #include "zUICustom.cpp"
 #include "zUIController.cpp"
 #include "zUIUserString.cpp"
